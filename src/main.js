0173690247856862973279852e47c4a04e413c5b
Vitalii <ilovepinkpony1@gmail.com>
2019-05-28 00:29:55 +0300

    add spaces

--- src/main.js
+++ src/main.js
@@ -3,33 +3,33 @@
 window.addEventListener('load', sortTable);
 
 function sortTable() {
-const table = document.querySelector('.table');
-const thead = table.querySelector('thead');
-const tbody = table.querySelector('tbody');
+  const table = document.querySelector('.table');
+  const thead = table.querySelector('thead');
+  const tbody = table.querySelector('tbody');
 
-thead.addEventListener('click', () => {
-  const arr = [];
-    [...tbody.rows].forEach(tr => {
-    const result =  [...tr.cells].map(td => {
-      return td.innerText;
+  thead.addEventListener('click', () => {
+    const arr = [];
+      [...tbody.rows].forEach(tr => {
+      const result =  [...tr.cells].map(td => {
+        return td.innerText;
+      })
+      arr.push(result)
     })
-    arr.push(result)
-  })
-  
-  arr.sort((a, b) => {
-    if (Number(a[0]) > Number(b[0])) {
-      return 1;
-  }
-    if (Number(a[0]) < Number(b[0])) {
-      return -1;
-  }
-      return 0;
-  });
+    
+    arr.sort((a, b) => {
+      if (Number(a[0]) > Number(b[0])) {
+        return 1;
+    }
+      if (Number(a[0]) < Number(b[0])) {
+        return -1;
+    }
+        return 0;
+    });
 
-  [...tbody.rows].forEach((tr, index) => {
-    [...tr.cells].forEach((td, word) => {
-      td.innerText = arr[index][word];
+    [...tbody.rows].forEach((tr, index) => {
+      [...tr.cells].forEach((td, word) => {
+        td.innerText = arr[index][word];
+      })
     })
   })
-})
 }
