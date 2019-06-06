f8cb13b047062fda6a580b8edc549da16374b083
Vitalii <ilovepinkpony1@gmail.com>
2019-06-06 12:52:47 +0300

    fixed issues

--- src/main.js
+++ src/main.js
@@ -8,31 +8,40 @@ function sortTable() {
   const tbody = table.querySelector('tbody');
 
   thead.addEventListener('click', () => {
+    const currSortCell = event.target.cellIndex;
+
     const arr = [...tbody.rows].map(tr => {
       return [...tr.cells].map(td => {
         return td.innerText;
-      })
-    })
-
-    arr.sort((a, b) => {
-      if (Number(a[0]) > Number(b[0])) {
-        return 1;
-    }
-      if (Number(a[0]) < Number(b[0])) {
-        return -1;
-    }
-        return 0;
+      });
     });
+
+    if (Number.isInteger(parseInt(arr[0][currSortCell]))) {
+      arr.sort((a, b) => {
+        if (Number(a[currSortCell]) > Number(b[currSortCell])) {
+          return 1;
+        }
+        if (Number(a[currSortCell]) < Number(b[currSortCell])) {
+          return -1;
+        }
+        return 0;
+      });
+    } else {
+      arr.sort((a, b) => {
+        if (a[currSortCell].toLowerCase() > b[currSortCell].toLowerCase()) {
+          return 1;
+        }
+        if (a[currSortCell].toLowerCase() < b[currSortCell].toLowerCase()) {
+          return -1;
+        }
+        return 0;
+      });
+    }
 
     [...tbody.rows].forEach((tr, index) => {
       [...tr.cells].forEach((td, word) => {
         td.innerText = arr[index][word];
-      })
-    })
-
-
-    
-    console.log(arr);
-    
-  })
+      });
+    });
+  });
 }
